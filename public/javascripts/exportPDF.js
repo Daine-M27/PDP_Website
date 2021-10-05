57adc4552f254762e6de7034a0c59c9c48b78fae
Daine Marshall <dmarshall@thelightsource.com>
2021-10-05 06:52:12 -0400

    added pdf download support

--- public/javascripts/exportPDF.js
+++ public/javascripts/exportPDF.js
@@ -1,10 +1,26 @@
 function downloadPDF() {
+  var element = document.getElementById('svgBody')
+  var opt = {
+    margin: 0,
+    filename: 'MEGABATTEN Submittal',
+    html2canvas: {
+      scale: 2
+    },
+    image: {
+      type: 'jpeg',
+      quality: .60
+    },
+    jsPDF: {
+      unit: 'in',
+      format: [11.05, 17.05],
+      orientation: 'landscape',
+      compress: true
+    },
+    pagebrake: {
+      mode: 'css',
+      before: ['#sheet2','#sheet3','#sheet4','#sheet5']
+    }
+  }
 
-  svgExport.downloadPdf(document.getElementById('sheet1'),'TitleHere' 
-  );
+  html2pdf().set(opt).from(element).save()
 }
-// document.querySelector("#btn_export_pdf").onclick = function(){
-//   svgExport.downloadPdf(document.querySelector("#sheet1"), null, {
-//     pdfOptions: {},
-//   });
-// };
