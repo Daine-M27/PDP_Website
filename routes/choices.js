bdf8653ebe84a3e902da572ce2a70ac50e54f8c2
Daine Marshall <dmarshall@thelightsource.com>
2021-11-03 08:53:46 -0400

    working first and next choice route

--- routes/choices.js
+++ routes/choices.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const choices = require('../models/ProductComponentOptions')
 
 
-/* GET choices listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET first choice listing. */
+router.get('/firstChoice', async function(req, res) {
+  try {
+    const firstChoice = await choices.findOne({ ComponentTypeName: 'Series'}).exec()
+    res.json(firstChoice)
+  } catch (error) {
+    res.status(400).send('Error getting first choice')
+  }
 });
+
+/* GET next choice listing. */
+router.get('/nextChoice/:ChildDecisionNodeID', async function(req, res) {
+  console.log(req.params)
+  try {
+    const nextChoice = await choices.findOne({ ParentDecisionNodeID: req.params.ChildDecisionNodeID}).exec()
+    res.json(nextChoice)
+  } catch (error) {
+    res.status(400).send('Error getting next choice')
+  }
+});
+
+
+
 
 module.exports = router;
