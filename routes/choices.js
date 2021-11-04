356e7c1ea895da336e0dbc8b075a0cdc65730be4
Daine Marshall <dmarshall@thelightsource.com>
2021-11-04 10:57:25 -0400

    minor changes to app.js and choices route

--- routes/choices.js
+++ routes/choices.js
@@ -3,21 +3,20 @@ const router = express.Router();
 const choices = require('../models/ProductComponentOptions')
 
 
-/* GET first choice listing. */
+/* GET first choice */
 router.get('/firstChoice', async function(req, res) {
   try {
-    const firstChoice = await choices.findOne({ ComponentTypeName: 'Series'}).exec()
+    const firstChoice = await choices.findOne({ ComponentTypeName: 'Series' }).exec()
     res.json(firstChoice)
   } catch (error) {
     res.status(400).send('Error getting first choice')
   }
 });
 
-/* GET next choice listing. */
+/* GET next choice */
 router.get('/nextChoice/:ChildDecisionNodeID', async function(req, res) {
-  console.log(req.params)
   try {
-    const nextChoice = await choices.findOne({ ParentDecisionNodeID: req.params.ChildDecisionNodeID}).exec()
+    const nextChoice = await choices.findOne({ ParentDecisionNodeID: req.params.ChildDecisionNodeID }).exec()
     res.json(nextChoice)
   } catch (error) {
     res.status(400).send('Error getting next choice')
