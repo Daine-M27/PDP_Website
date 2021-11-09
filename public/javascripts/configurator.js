b94bf850362b9c4e05145072f07d74d0572fc00b
Daine Marshall <dmarshall@thelightsource.com>
2021-11-09 07:32:55 -0500

    working basic choice structure

--- public/javascripts/configurator.js
+++ public/javascripts/configurator.js
@@ -1,5 +1,6 @@
-var getNextChoice = async (id) => {
-  var ChildDecisionNodeID = $(`#${id} option:selected`).val()
+// get next choice from api
+const getNextChoice = async (id) => {
+  const ChildDecisionNodeID = $(`#${id} option:selected`).val()
   console.log(ChildDecisionNodeID)
   await $.get(`/choices/nextChoice/${ChildDecisionNodeID}`, (data) => {
     console.log(data)
@@ -9,35 +10,43 @@ var getNextChoice = async (id) => {
 }
 
 // add new choice when called
-var addChoiceRow = (choiceData) => {
+const addChoiceRow = (choiceData) => {
   // get main container for innerHTML operation
-  var choiceContainer = document.getElementById('contents')
+  const choiceContainer = document.getElementById('contents')
+  const elemId = `choice${choiceData.ParentDecisionNodeID}`
   
   // create option for each catalog in componenet
-  var pulldownOptions = (id) => {    
+  const pulldownOptions = (id) => {    
     choiceData.Catalogs.forEach(catalog => {
+      console.log(catalog)
       $(`#${id}`).append(`<option class="select-option" value="${catalog.ChildDecisionNodeID}">${catalog._catalogDescription}</option>`)      
     });
   }
   
   // setup main select element to append data
-  choiceContainer.innerHTML = 
-  `<div id=${choiceData.ParentDecisionNodeID}>
-      <h2 style='clear:left;margin-top:0em;'>${choiceData.ComponentTypeName}</h2>
-      <select id='choice${choiceData.ComponentTypeName}' name='${choiceData.ComponentTypeName}' onchange='getNextChoice("choice${choiceData.ComponentTypeName}")'>
-        <option disabled selected class='select-option' value='' > -- select and option -- </option>
+  choiceContainer.innerHTML += 
+  `<div style='margin-bottom:.5em' id=${choiceData.ParentDecisionNodeID}>
+      <h2 style='clear:left;margin-top:0em; margin-bottom:0em'>${choiceData.ComponentTypeName}</h2>
+      <select id='${elemId}' name='${choiceData.ComponentTypeName}' onchange='getNextChoice("${elemId}")'>
+        <option disabled selected class='select-option' value='' > -- select and option -- </option>        
       </select>
     </div>`
   
   // add options to select element
-  pulldownOptions(`choice${choiceData.ComponentTypeName}`)
+  pulldownOptions(elemId)
 }
 
 
 // initial choice loaded on page load
 window.onload = async () => {
+  
+  // await $.get(`/choices/nextChoice/2033`, (data) => {
+  //   console.log(data)
+  //   addChoiceRow(data)
+  // })
 
   await $.get('/choices/firstChoice', (data) => {
+    // console.log(data)
     addChoiceRow(data)
   })
 }
