5be57afc6de21fe2c4c3d2da600a1d507d09a409
Daine Marshall <dmarshall@thelightsource.com>
2021-12-07 11:07:20 -0500

    added redirect for default route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 /* GET configurator page. */
+router.get('/', function(req, res, next) {
+  res.redirect('/configurator')
+});
+
 router.get('/configurator', function(req, res, next) {
   res.render('configurator', { pageTitle: 'MEGABATTEN Configurator' });
 });
