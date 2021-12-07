2f3c097afefbf0184086f4ec10b1106ca8ddfd06
Daine Marshall <dmarshall@thelightsource.com>
2021-12-07 12:48:53 -0500

    removed / route

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 /* GET configurator page. */
-router.get('/', function(req, res, next) {
-  res.redirect('/configurator')
-});
+// router.get('/', function(req, res, next) {
+//   res.redirect('/configurator')
+// });
 
 router.get('/configurator', function(req, res, next) {
   res.render('configurator', { pageTitle: 'MEGABATTEN Configurator' });
