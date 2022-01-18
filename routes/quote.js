33e3ec7e8030a0e3fff7b817cfd5c9194f950082
Daine Marshall <dmarshall@thelightsource.com>
2022-01-18 17:13:55 -0500

    changed name of utility file for partNumbers

--- routes/quote.js
+++ routes/quote.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { partNumbers } = require('../utilities/quotePage');
+const { partNumbers } = require('../utilities/getPartNumbers');
 const quoteRequest = require('../models/QuoteRequests')
 const mail = require('../utilities/email')
 
