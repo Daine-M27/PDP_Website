231bc295e4d1f6b941b9c3b1f92d9d0dd6bd08de
Daine Marshall <dmarshall@thelightsource.com>
2022-01-27 14:14:42 -0500

    scroll on drawing page

--- public/javascripts/drawingLoader.js
+++ public/javascripts/drawingLoader.js
@@ -13,22 +13,27 @@ window.onload = async () => {
     await $.get(`/drawing/loader/sheet1/${id}`, (data) => {
       loaderOff('sheet1')
       document.getElementById('sheet1').insertAdjacentHTML('beforeend', data);
+      window.scrollTo(0, document.body.scrollHeight)
     });
     await $.get(`/drawing/loader/sheet2/${id}`, (data) => {
       loaderOff('sheet2')      
       document.getElementById('sheet2').insertAdjacentHTML('beforeend', data);
+      window.scrollTo(0, document.body.scrollHeight)
     });
     await $.get(`/drawing/loader/sheet3/${id}`, (data) => {      
       loaderOff('sheet3')
       document.getElementById('sheet3').insertAdjacentHTML('beforeend', data);
+      window.scrollTo(0, document.body.scrollHeight)
     });
     await $.get(`/drawing/loader/sheet4/${id}`, (data) => {      
       loaderOff('sheet4')
       document.getElementById('sheet4').insertAdjacentHTML('beforeend', data);
+      window.scrollTo(0, document.body.scrollHeight)
     });
     await $.get(`/drawing/loader/sheet5/${id}`, (data) => {      
       loaderOff('sheet5')
       document.getElementById('sheet5').insertAdjacentHTML('beforeend', data);
+      window.scrollTo(0, document.body.scrollHeight)
     });
 
 
