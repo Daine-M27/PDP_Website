44de5708293c34e011d7fafd3f54298f10b2e857
Daine Marshall <dmarshall@thelightsource.com>
2022-02-21 14:10:02 -0500

    added env for cache settings

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const views = path.join(__dirname, 'views')
 app.set('views', views );
 app.set('view engine', 'pug');
 
-const cache = new CachePugTemplates({ app, views })
+const cache = new CachePugTemplates({ app, views, concurrency: process.env.CONCURRENCY, interval: process.env.INTERVAL })
 cache.start();
 
 app.use(compression());
